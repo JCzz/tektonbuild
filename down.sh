12bad8f7155926e98a7bc86a7629563e0be1ea95
GlobalAwear <awear@appsyouwear.com>
2020-01-07 17:02:17 +0100

    sub folders

--- down.sh
+++ down.sh
@@ -3,5 +3,3 @@ kubectl delete -f taskrun.yaml
 kubectl delete -f pipres_gcr.yaml
 kubectl delete -f pipres_github.yaml
 kubectl delete -f sa.yaml
-tkn taskrun delete build-docker-image-from-git-source
-tkn taskrun delete build-docker-image-from-git-source-task-run
